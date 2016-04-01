a0619eba85cd1249d4365f5efba7f47abe7d7af8
frate <paolo@valier.it>
2016-04-01 09:54:18 +0200

    first commit

---
+++ __openerp__.py
@@ -0,0 +1,16 @@
+{
+  'name':'Theme for valier.it',
+  'description': 'Theme for valier.it website',
+  'version':'0.1',
+  'author':'Paolo Valier',
+
+  'data': [
+	'views/layout.xml',
+	'views/assets.xml',
+	'views/snippets.xml',
+	'views/options.xml'
+  ],
+
+  'category': 'Theme/Creative',
+  'depends': ['website', 'website_less','website_sale', 'website_blog'],
+}
